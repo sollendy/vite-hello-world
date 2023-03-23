c5b0984d6a383589e1e7989b483e356591683032
sollendy <edlannino@tutanota.com>
2023-03-23 16:50:33 +0100

    ancora problemi con la scritta

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import title from "./components/AppTitle.vue"
+// import HelloWorld from './components/HelloWorld.vue';
+export default {
+    data(){
+      return{
+        
+      }
+    },
+    
+    components: {
+      title,
+    }
+}
+</script>
+
+<template>
+  <title></title>
+</template>
+
+<style scoped>
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+</style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,3 +1,12 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
 
 <template>
     <h1>La mia prima app con Vite!</h1>
